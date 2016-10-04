e53936ea6b5751c942c3a8da516247f23d68deb2
Den Volkov <volkovdenya@gmail.com>
2016-10-04 11:32:37 +0300

    TC-153 change background color of btn

--- protected/humhub/modules/user/views/mails/UserInviteSpace.php
+++ protected/humhub/modules/user/views/mails/UserInviteSpace.php
@@ -126,33 +126,33 @@ use humhub\models\Setting;
                                             </td>
                                         </tr>
                                     </table>
+                                    </td>
+                                    </tr>
+                                    <!-- end  content top-->
+
+
+                                    <!--start space height -->
+                                    <tr>
+                                        <td height="5" class="col-underline"></td>
+                                    </tr>
+                                    <!--end space height -->
+
+
+                                </table>
+                                <!-- end content left -->
+
+
                             </td>
                         </tr>
-                        <!-- end  content top-->
-
-
-                        <!--start space height -->
-                        <tr>
-                            <td height="5" class="col-underline"></td>
-                        </tr>
-                        <!--end space height -->
-
+                        <!-- end image and content -->
 
                     </table>
-                    <!-- end content left -->
-
-
+                    <!-- end  container width 560px -->
                 </td>
             </tr>
-            <!-- end image and content -->
-
         </table>
-        <!-- end  container width 560px -->
+        <!-- end  container width 600px -->
     </td>
-</tr>
-</table>
-<!-- end  container width 600px -->
-</td>
 </tr>
 
 <tr>
@@ -218,7 +218,7 @@ use humhub\models\Setting;
                                             <!-- start button -->
                                             <table border="0" align="center" cellpadding="0" cellspacing="0">
                                                 <tr>
-                                                    <td width="auto"  align="center" valign="middle" height="32" style=" background-color:<?php echo Setting::Get('colorPrimary'); ?>;  border-radius:5px; background-clip: padding-box;font-size:14px; font-family:Open Sans, Arial,Tahoma, Helvetica, sans-serif; text-align:center;  color:#ffffff; font-weight: 600; padding-left:30px; padding-right:30px; padding-top: 5px; padding-bottom: 5px;">
+                                                    <td width="auto"  align="center" valign="middle" height="32" style=" background-color:#2c305d;  border-radius:5px; background-clip: padding-box;font-size:14px; font-family:Open Sans, Arial,Tahoma, Helvetica, sans-serif; text-align:center;  color:#ffffff; font-weight: 600; padding-left:30px; padding-right:30px; padding-top: 5px; padding-bottom: 5px;">
 
                                                         <span style="color: #ffffff; font-weight: 300;">
                                                             <a href="<?php echo Url::to(['/user/auth/create-account', 'token' => $token], true); ?>" style="text-decoration: none; color: #ffffff; font-weight: 300;">
