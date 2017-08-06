e2188f8361f3a05336f284cedb5926768ca4a3cd
Katarzyna_Fedevych <katarzynan@gmail.com>
2017-08-06 16:29:36 +0200

    TC-245 - fixing mailing errors

--- protected/humhub/modules/space/notifications/views/mail/plaintext/inviteDeclined.php
+++ protected/humhub/modules/space/notifications/views/mail/plaintext/inviteDeclined.php
@@ -2,8 +2,16 @@
 
 use yii\helpers\Html;
 
+if (!empty($originator) && is_object($originator) && !empty($originator->displayName)) {
+    $displayName = Html::encode($originator->displayName);
+} else {
+    $displayName = '(N/A)';
+}
+
 echo strip_tags(Yii::t('SpaceModule.views_notifications_inviteDeclined', '{userName} declined your invite for the space {spaceName}', array(
-    '{userName}' => Html::encode($originator->displayName),
+    '{userName}' => $displayName,
     '{spaceName}' => Html::encode($source->name)
 )));
+
 ?>
+
